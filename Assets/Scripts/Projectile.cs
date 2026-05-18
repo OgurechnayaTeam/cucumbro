47584ed4413e0c5c32fd090f133649321bc1ae9d
Danilova Darya <da-danilova@mail.ru>
2026-05-18 21:04:38 +0500

    gun + katana

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float speed = 10f; // Скорость полёта
+    [SerializeField] float lifetime = 3f; // Время жизни
+
+    private Vector2 direction;
+
     void Start()
     {
-        
+        // Удаляем пулю через lifetime секунд
+        Destroy(gameObject, lifetime);
+
+        // Направление берём из текущего поворота объекта
+        direction = transform.right; // Если пуля смотрит вправо по умолчанию
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Двигаем пулю в направлении её поворота
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    // Если нужно наносить урон при столкновении
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit Enemy!");
+            // Здесь можно вызвать метод нанесения урона у врага
+            // other.GetComponent<Enemy>().TakeDamage(10);
+            Destroy(gameObject); // Уничтожаем пулю при попадании
+        }
     }
 }
